74203efdf0f84530807dc33c05d06c04eb7c7523
Wanli Yang <yangwanli@stu.pku.edu.cn>
2025-02-23 13:45:07 +0800

    完善focus功能

--- miniprogram/pages/focus/focus.js
+++ miniprogram/pages/focus/focus.js
@@ -4,9 +4,8 @@ Page({
   data: {
     focusTime: null, // 用户输入的专注时间（分钟）
     isCounting: false, // 是否正在倒计时
-    timer: null, // 定时器
-    countdown: '00:00:00', // 倒计时显示
-    leftTime: null, // 剩余时间（秒）
+    startTime: null, // 专注开始时间
+    time: 0, // 剩余时间（秒）
   },
 
   // 监听用户输入
@@ -20,61 +19,19 @@ Page({
   startCountdown() {
     if (!this.data.focusTime || this.data.isCounting) return;
 
-    const focusTimeInSeconds = this.data.focusTime * 60; // 将分钟转换为秒
     const startTime = new Date();
+    const focusTimeInMilliseconds = this.data.focusTime * 60 * 1000; // 将分钟转换为毫秒
     
     this.setData({
       isCounting: true,
-      startTime: startTime
+      startTime: startTime,
+      time: focusTimeInMilliseconds,
     });
-
-    let remainingTime = focusTimeInSeconds;
-    this.updateCountdown(remainingTime);
-
-    // 启动定时器
-    this.data.timer = setInterval(() => {
-      remainingTime--;
-      if (remainingTime >= 0) {
-        this.updateCountdown(remainingTime);
-        this.setData({
-          leftTime: remainingTime
-        });
-      } else {
-        this.stopCountdown();
-        wx.showToast({
-          title: '专注时间结束！',
-          icon: 'none',
-        });
-      }
-    }, 1000);
-  },
-
-  // 更新倒计时显示
-  updateCountdown(seconds) {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const secs = seconds % 60;
-    const countdown = `${this.formatTime(hours)}:${this.formatTime(minutes)}:${this.formatTime(secs)}`;
-    this.setData({
-      countdown,
-    });
-  },
-
-  // 格式化时间（补零）
-  formatTime(time) {
-    return time < 10 ? `0${time}` : time;
   },
 
   // 停止倒计时
-  stopCountdown() {
+  finishCountdown() {
     const userId = wx.getStorageSync('userId');
-    const focusDurationInSec = this.data.focusTime * 60;
-
-    clearInterval(this.data.timer);
-    this.setData({
-      isCounting: false,
-      countdown: '00:00:00',
-    });
 
     wx.request({
       url: apiConfig.creatFocusUrl,
@@ -82,7 +39,7 @@ Page({
       data: {
         userId: userId,
         startTime: this.data.startTime,
-        focusDuration: focusDurationInSec
+        focusDuration: this.data.focusTime
       },
       success: (res) => {
         return res.data;
@@ -94,6 +51,11 @@ Page({
           icon: 'none',
         });
       },
+    });
+
+    this.setData({
+      focusTime: null,
+      isCounting: false,
     });
   },
 });
